3fa923acfc22c5355cc006a9d7f52e0ce0771dcf
sunitha <sunithapraveenkumar@gmail.com>
2022-11-24 12:47:15 +0530

    1. Completed SplashActivity.java

---
+++ app/src/main/java/com/improve10x/advancedtemplate/TemplateActivity.java
@@ -0,0 +1,18 @@
+package com.improve10x.advancedtemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class TemplateActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_template);
+    }
+}
