cc318809040971a6c2328f5d396a63138bcb24f1
sunitha <sunithapraveenkumar@gmail.com>
2022-11-24 14:58:49 +0530

    1. completed TemplateAdapter.java

--- app/src/main/java/com/improve10x/advancedtemplate/TemplateAdapter.java
+++ app/src/main/java/com/improve10x/advancedtemplate/TemplateAdapter.java
@@ -1,5 +1,7 @@
 package com.improve10x.advancedtemplate;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -8,8 +10,31 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class TemplateAdapter extends RecyclerView.Adapter <TemplateViewHolder> {
+    public  List<Template> templates;
+    public void setData(List<Template>templateList) {
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public TemplateViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.template_item,parent,false);
+        TemplateViewHolder templateViewHolder = new TemplateViewHolder(view);
+        return templateViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TemplateViewHolder holder, int position) {
+        Template template = templates.get(position);
+        holder.message.setText(template.message);
 
     }
+
+    @Override
+    public int getItemCount() {
+        return templates.size();
+    }
+}
 
 
 
